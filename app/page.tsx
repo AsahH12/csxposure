25bad4bf350aaa59ba0daa0c298de17337702607
s_cyi <asahhayes@gmail.com>
2025-01-25 15:12:13 -0500

    firebase files

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
-}
+'use client';
+import {useRouter} from 'next/navigation'
+const Page1: React.FC = () => {
+  const router = useRouter();
+  const navigateToPageSignUPTemp = () => {
+    router.push('/SignUp');
+  };
+
+  return (
+    <div className='p-4 text-center'>
+      <h1 className='text-2xl font-bold'>Page 1
+        <button 
+        onClick={navigateToPageSignUPTemp}
+        className='mt-4 bg blue-500 text-white py-2 px-4 rounded-lg'>
+          Go To Page 2
+        </button>
+      </h1>
+    </div>
+  );
+};
+export default Page1;
