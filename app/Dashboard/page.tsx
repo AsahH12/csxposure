db7521ebff93270d1ed3c18f4f62ab0caeefa6ba
s_cyi <asahhayes@gmail.com>
2025-01-25 15:37:32 -0500

    Basic Home and dashpage, needed to deploy on firebase

---
+++ app/Dashboard/page.tsx
@@ -0,0 +1,8 @@
+// page.tsx
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+  return <div>Dashboard</div>;
+};
+
+export default DashboardPage;  // Ensure you're exporting it as a module
---
+++ app/Home/page.tsx
@@ -0,0 +1,8 @@
+// page.tsx
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return <div>Home</div>;
+};
+
+export default HomePage;  // Ensure you're exporting it as a module
