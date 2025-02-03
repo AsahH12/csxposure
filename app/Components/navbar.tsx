68bc906d6868e16a91855211712491afa8065041
Payton De Veyra <deveyrapayton@gmail.com>
2025-02-03 01:23:15 -0500

    Navbar notification and dropdown profile

--- app/Components/navbar.tsx
+++ app/Components/navbar.tsx
@@ -1,45 +1,80 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
-// import './navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 const profilePhotoUrl = '/placeholder-profile.jpg';
 
 const Navbar: React.FC = () => {
-    return (
-        <nav className="navbar navbar-dark bg-primary text-white p-4">
-        <div className="navbar-start">
-            <div className="navbar-logo">
-                <Link href="/Home" className="text-white">
-                        <img
-                            src="/logo.png"
-                            alt="Logo"
-                            width={210} 
-                            height={50}
-                            className=""
-                        />
-                </Link>
-            </div>
-        </div>
-        <ul className="navbar-end">
-        <Link href="/Authentication" className="text-white mx-2">
+  // Dynamically import Bootstrap's JavaScript on the client side
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min');
+  }, []);
+
+  return (
+    <nav className="navbar navbar-dark bg-primary text-white p-4">
+      <div className="navbar-start">
+        <div className="navbar-logo">
+          <Link href="/Home" className="text-white">
             <img
-              src="placeholder-profile.jpg" // Placeholder for now, will be replaced with Firebase URL
+              src="/logo.png"
+              alt="Logo"
+              width={210}
+              height={50}
+              className=""
+            />
+          </Link>
+        </div>
+      </div>
+      <ul className="navbar-end d-flex align-items-center ">
+
+         {/* Profile Dropdown */}
+         <div className="nav-item dropdown mx-2">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <img
+              src={profilePhotoUrl}
               alt="Profile"
               width={50}
               height={50}
-              className="rounded-circle" 
+              className="rounded-circle"
             />
-        </Link>
-            {/* <Link href="/Authentication" className="text-white mx-2">
-                Profile
-            </Link> */}
-            <Link href="/" className="text-white mx-2">
-                Notifications
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <Link href="/Authentication" className="dropdown-item">
+              Login/SignUp
             </Link>
-        </ul>
+            <Link href="/ProfileEditPage" className="dropdown-item">
+              My Profile
+            </Link>
+            <div className="dropdown-divider"></div>
+            <Link href="/logout" className="dropdown-item">
+              Logout
+            </Link>
+          </div>
+        </div>
+        {/* Notification Icon */}
+        <div className="nav-item">
+          <Link href="/" className="text-white mx-2 me-5">
+            <img
+              src="Notification_White_False.png"
+              alt="Notifications"
+              width={50}
+              height={50}
+            />
+          </Link>
+        </div>
+
+      </ul>
     </nav>
-);
+  );
 };
 
 export default Navbar;
